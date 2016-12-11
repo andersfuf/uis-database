16d2d47123c293b03549a61b4af92e30951c75e9
Anders Lassen <develop@lassena.dk>
2016-12-11 14:27:28 +0100

    db-create scripts

---
+++ system-ddl/b007_create_db-uis.sql
@@ -0,0 +1,17 @@
+﻿--AL20161211
+-- removing
+DROP DATABASE uis;
+-- User: uis
+DROP ROLE IF EXISTS uis;
+
+CREATE USER uis WITH PASSWORD 'uis';
+
+-- Database: uis
+CREATE DATABASE uis;
+COMMENT ON DATABASE uis  IS 'Development of Information Systems (UIS) Sample databases';
+
+-- User priviledges
+GRANT CONNECT, TEMPORARY ON DATABASE uis TO public;
+GRANT ALL ON DATABASE uis TO uis;
+GRANT ALL ON DATABASE uis TO develop;
+
